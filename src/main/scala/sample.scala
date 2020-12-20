f03c43771c64c0c1192bca24ededeeb0999731d9
john imperial <imperialjohncarlo@gmail.com>
2020-12-20 15:02:31 +0900

    new sample package

--- src/main/scala/sample.scala
+++ src/main/scala/sample.scala
@@ -1,10 +1,12 @@
 import utils.Messenger.Greet
+import utils.SampleAPI.Caller.call
 
 
 // You can name the object same as project name or app name
 object sample {
   def main(args: Array[String]): Unit = {
     // println("Welcome to my scala program!")
-    Greet()
+    // Greet()
+    call()
   }
 }
---
+++ src/main/scala/utils/SampleAPI/Caller.scala
@@ -0,0 +1,9 @@
+package utils.SampleAPI
+
+
+// package practice implementation
+object Caller {
+    def call(): Unit = {
+        println("calling from SampleAPI")
+    }
+}
