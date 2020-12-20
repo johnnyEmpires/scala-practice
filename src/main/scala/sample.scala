3341c30b33efd8c9c99c7f096fa65c10b61fb328
johnnyempires <imperialjohncarlo@gmail.com>
2020-12-20 21:44:14 +0800

    ignore data folder

--- src/main/scala/sample.scala
+++ src/main/scala/sample.scala
@@ -1,6 +1,7 @@
 import utils.Messenger.Greet
 import utils.SampleAPI.Caller.call
 import utils.SampleAPI.Caller.looper
+import utils.SampleAPI.Caller.openFile
 
 
 // You can name the object same as project name or app name
@@ -9,7 +10,8 @@ object sample {
     // println("Welcome to my scala program!")
     // Greet()
     // call()
-    looper()
+    // looper()
+    openFile()
 
   }
 }
--- src/main/scala/utils/SampleAPI/Caller.scala
+++ src/main/scala/utils/SampleAPI/Caller.scala
@@ -6,17 +6,25 @@ import com.typesafe.scalalogging.Logger
 
 // package practice implementation
 object Caller {
+
+    val logger = Logger("Root")
+
     def call(): Unit = {
         println("calling from SampleAPI")
         }
 
     def looper(): Unit = {
-
-        val logger = Logger("Root")
-
-        for( a <- 1 to 10) {
+        for( a <- 1 to 10000) {
             // println( "Value of a: " + a );
             logger.info("Value of a: " + a)
         }
+    }
+
+    def openFile(): Unit = {
+        val bs = io.Source.fromFile("./data/data-latest.csv")
+        for (line <- bs.getLines) {
+            // logger.info(line)
+        }
+
     }
 }
