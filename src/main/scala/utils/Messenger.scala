e7492016138612cd85af4756f976df281147f7a8
john imperial <imperialjohncarlo@gmail.com>
2020-12-20 14:32:44 +0900

    ignore .bloop folder

--- src/main/scala/utils/Messenger.scala
+++ src/main/scala/utils/Messenger.scala
@@ -6,5 +6,8 @@ object Messenger {
     def Greet() : Unit = {
         println("This is a message.")
         println("another neeeeeeeeeew message!!")
+        println("another neeeeeeeeeew message!!")
+        println()
+        println("another message!")
     }
 }
