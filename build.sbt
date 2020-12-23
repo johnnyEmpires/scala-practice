386035a996a1db253ef9a29766e8b136f313d1b9
johnnyempires <imperialjohncarlo@gmail.com>
2020-12-23 21:40:28 +0800

    added sbt-native-packager

--- build.sbt
+++ build.sbt
@@ -1,2 +1,4 @@
 libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.8.0"
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.1.3"
+
+enablePlugins(JavaAppPackaging)
--- src/main/scala/sample.scala
+++ src/main/scala/sample.scala
@@ -7,11 +7,11 @@ import utils.SampleAPI.Caller.openFile
 // You can name the object same as project name or app name
 object sample {
   def main(args: Array[String]): Unit = {
-    // println("Welcome to my scala program!")
+    println("Welcome to my scala program!")
     // Greet()
-    // call()
+    call()
     // looper()
-    openFile()
+    // openFile()
 
   }
 }
