4a0080d7d8eb9ea836d4dfb93a1848939693b295
john <imperialjohncarlo@gmail.com>
2021-02-16 06:54:34 +0900

    sleep

--- src/main/scala/sample.scala
+++ src/main/scala/sample.scala
@@ -1,5 +1,5 @@
 import utils.Messenger.Greet
-import utils.SampleAPI.Caller.call
+import utils.SampleAPI.Caller
 import utils.SampleAPI.Caller.looper
 import utils.SampleAPI.Caller.openFile
 
@@ -7,11 +7,20 @@ import utils.SampleAPI.Caller.openFile
 // You can name the object same as project name or app name
 object sample {
   def main(args: Array[String]): Unit = {
+
+  	// blank line separator
+  	println();
+
     println("Welcome to my scala program!")
     // Greet()
-    // call()
-    looper()
+    Caller.call()
+    // looper()
     // openFile()
+
+    Thread.sleep(1000);
+
+  	// blank line separator
+  	println();
 
   }
 }
